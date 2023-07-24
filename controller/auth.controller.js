5f764ad51becaae0fd19c95bd288f20042f790fc
developerdesigner18 <developer.designer18@gmail.com>
2023-07-24 15:28:46 +0530

    create api for get user data

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,8 +1,14 @@
+const User = require('../models/user');
+
 const getUserData = async (req, res) => {
   try {
     console.log(req.body);
+    let data = await User.findById(req.userId);
+    console.log(data);
+    res.send(200).json({ success: true, data: data });
   } catch (error) {
     console.log(error);
+    res.send(400).json({ success: false });
   }
 };
 
