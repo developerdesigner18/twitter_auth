424748463c66a7e26333a70eb274ef4107331f6e
developerdesigner18 <developer.designer18@gmail.com>
2023-07-24 15:20:46 +0530

    create api for get user data

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,15 @@
 const router = require("express").Router();
 const passport = require("passport");
 const { CLIENT_URL } = require("../config/globals");
+const { getUserData } = require('../controller/auth.controller');
 
 // send profile data if user is logged in
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   console.log(req.cookies, 'req');
   res.status(200).json({ user: req.user });
 });
 
-router.get("/logout", (req, res) => {
+router.get('/logout', (req, res) => {
   req.logout();
   // req.session.destroy(); // delete session from db after logout
   res.redirect(CLIENT_URL);
@@ -18,7 +19,7 @@ router.get('/env', (req, res) => {
   console.log('env', process.env.TWITTER_CONSUMER_KEY);
 });
 
-router.get("/login", passport.authenticate("twitter"));
+router.get('/login', passport.authenticate('twitter'));
 
 router.get(
   '/redirect',
@@ -31,5 +32,7 @@ router.get(
     res.redirect(`${CLIENT_URL}?user=${req.session.passport.user}`);
   }
 );
+
+router.post('/getUserData', getUserData);
 
 module.exports = router;
